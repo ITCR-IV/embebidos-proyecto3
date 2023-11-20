31a5b80b55ba7f0e2fb5b05f32905b85b94ac5c0
Ignacio Vargas <ignaevc@gmail.com>
2023-11-20 16:31:23 -0600

    Hacer que nix se conecte a cel

--- nixos/triviOS/cli.nix
+++ nixos/triviOS/cli.nix
@@ -3,6 +3,7 @@
     environment.systemPackages = with pkgs; [
       libraspberrypi
       libgpiod
+			vim
     ];
 
     users.users."root".initialPassword = "password";
--- nixos/triviOS/net.nix
+++ nixos/triviOS/net.nix
@@ -1,15 +1,21 @@
 {
   config = {
     networking = {
-      hostName = "tripi";
+      hostName = "rpi3";
       firewall.allowedTCPPorts = [ 80 ];
 
       wireless = {
         enable = true;
-        environmentFile = "/var/lib/wlan.conf";
+        environmentFile = "/etc/wlan.conf";
         networks."@WLAN_SSID@".psk = "@WLAN_PSK@";
       };
     };
+
+    # Por default conectar a cel de Ignacio
+    environment.etc."wlan.conf".text = ''
+            		WLAN_SSID=cascade
+            		WLAN_PSK=12345678
+      					'';
 
     services.openssh = {
       enable = true;
