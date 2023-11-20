313b1935db3225b0b3592b760660d64ac7c78493
Ignacio Vargas <ignaevc@gmail.com>
2023-11-20 17:52:10 -0600

    Hacer triviOS funcionar como hotspot para conectarse a él

--- nixos/triviOS/net.nix
+++ nixos/triviOS/net.nix
@@ -2,25 +2,51 @@
   config = {
     networking = {
       hostName = "rpi3";
-      firewall.allowedTCPPorts = [ 80 ];
+      firewall =
+        {
+          allowedTCPPorts = [ 80 ];
+          allowedUDPPorts = [ 53 67 ]; # DNS & DHCP
+        };
 
-      wireless = {
-        enable = true;
-        environmentFile = "/etc/wlan.conf";
-        networks."@WLAN_SSID@".psk = "@WLAN_PSK@";
-      };
+
+      interfaces.wlan0.ipv4.addresses = [
+        {
+          address = "192.168.1.1";
+          prefixLength = 16;
+        }
+      ];
+
+      wireless.enable = true;
+
+      # wireless = {
+      #   enable = true;
+      #   environmentFile = "/etc/wlan.conf";
+      #   networks."@WLAN_SSID@".psk = "@WLAN_PSK@";
+      # };
     };
 
-    # Por default conectar a cel de Ignacio
-    environment.etc."wlan.conf".text = ''
-            		WLAN_SSID=cascade
-            		WLAN_PSK=12345678
-      					'';
+    services = {
+      hostapd = {
+        enable = true;
+        interface = "wlan0";
+        wpa = true;
+        ssid = "triviOS";
+        wpaPassphrase = "12345678";
+      };
 
-    services.openssh = {
-      enable = true;
-      settings.PasswordAuthentication = true;
-      settings.PermitRootLogin = "yes";
+      dnsmasq = {
+        enable = true;
+        settings = {
+          domain-needed = true;
+          dhcp-range = [ "192.168.1.2,192.168.1.254" ];
+        };
+      };
+
+      openssh = {
+        enable = true;
+        settings.PasswordAuthentication = true;
+        settings.PermitRootLogin = "yes";
+      };
     };
   };
 }
